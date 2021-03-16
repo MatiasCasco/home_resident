c8db380e26f9d667b2ce02bbba356490415f3711
MatiasCasco <37419858+MatiasCasco@users.noreply.github.com>
2021-03-16 16:47:40 -0300

    Agregamos el widget Respuesta

---
+++ lib/widget/answer_container.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class AnswerContainer extends StatefulWidget {
+  @override
+  _AnswerContainerState createState() => _AnswerContainerState();
+}
+
+class _AnswerContainerState extends State<AnswerContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
