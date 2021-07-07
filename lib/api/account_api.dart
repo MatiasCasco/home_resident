e66b037baa6a3fe415cb0afb45f74afd9f0978d8
Alexis <achu.bti@gmail.com>
2021-07-07 15:49:24 -0400

    resuelto problema de doble consuta a log

--- lib/api/account_api.dart
+++ lib/api/account_api.dart
@@ -1,87 +1,3 @@
-/*
-import 'dart:convert';
-import 'package:http/http.dart' as http; // Alias
-
-class AccountAPI {
-  Future<bool> login(String email, String password) async{
-    try{
-      final  http.Response response = await http.post(
-          "https://reqres.in/api/login",
-          body: jsonEncode({"email": email, "password": password, "age":25}),
-          headers: {'Content-Type':'application/json'});
-      if(response.statusCode==200) {
-        final dynamic parsed = jsonDecode(response.body);
-        print("Login OK ${parsed["token"]}");
-        return true;
-      }
-      return false;
-      /*}else{
-      //print("StatusCode is ${response.statusCode}");
-    }*/
-    }catch(e){
-      print(e);
-    }
-  }
-  Future<List<dynamic>> getUsers(int page) async {
-    try {
-      final http.Response response = await http.get("https://reqres.in/api/users?page=$page&delay=3");
-      print(response.body);
-      if(response.statusCode==200) {
-        final parsed = jsonDecode(response.body);
-        print("getUsers ok");
-        print(parsed['data'].runtimeType); //averigua que tipo de dato retorna
-        return parsed['data'];
-
-      }
-      print("error ${response.statusCode}");
-      return [];
-    }catch(e){
-      print(e);
-      return [];
-    }
-  }
-
-/* Este es un ejemplo conectado a nuestro proyecto
-   Future<String> getCurso(String id) async {
-    try {
-      final http.Response response = await http.get(
-          "http://192.168.0.19:8084/homeApi/rest/cursoapi/curso/${id}");
-      print(response.body);
-      if (response.statusCode == 200) {
-        final parsed = jsonDecode(response.body);
-        print("getCurso");
-        print(parsed.runtimeType);
-        return parsed['nombre'];
-      }
-      print("error ${response.statusCode}");
-      return " ";
-    } catch(e){
-        print(e);
-        return " ";
-    }
-  }
-*/
-  /*
-  Future<Curso> getCurso(String id) async {
-    try {
-      final http.Response response = await http.get(
-          "http://192.168.0.3:8084/homeApi/rest/cursoapi/curso/${id}");
-      print(response.body);
-      if (response.statusCode == 200) {
-        final parsed = jsonDecode(response.body);
-        print("getCurso");
-        print(parsed.runtimeType);
-        Curso curso = new Curso.fromJson(parsed);
-        return curso;
-      }
-      print("error ${response.statusCode}");
-      return null;
-    } catch(e){
-      print(e);
-      return null;
-    }
-  }*/
-}*/
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart'; // Alias
@@ -89,11 +5,27 @@ import 'package:shared_preferences/shared_preferences.dart'; // Alias
 class AccountApi {
   _changeValue(parsed)async{
     SharedPreferences prFr = await SharedPreferences.getInstance();
-    await prFr.setBool("waslogin", true);
+    /*await prFr.setBool("waslogin", true);
     await prFr.setString("email",parsed['email']);
     await prFr.setString("nombreCurso",parsed['nombreCurso']);
-    await prFr.setInt("id",parsed['idP']);
-    print("cargando datos en memoria...");
+    await prFr.setInt("id",parsed['idP']);*/
+    final Map<String, dynamic> data1 = Map<String, dynamic>();
+    data1['alumno'] = parsed['idP'];
+    data1['curso'] = parsed['nombreCurso'];
+    data1['rol'] = parsed['rol'];
+    data1['email'] = parsed['email'];
+    data1['name'] = parsed['name'];
+    int idAlumno = data1["alumno"] as int;
+    String curso = data1["curso"].toString();
+    int rol = data1["rol"] as int;
+    String name =data1["name"].toString();
+    String email = data1["email"].toString();
+    await prFr.setString("Curso", curso);
+    await prFr.setInt("Alumno", idAlumno);
+    await prFr.setString("email", email);
+    await prFr.setString("name", name);
+    await prFr.setInt("rol", rol);
+   
   }
 
   Future<bool> login(String email, String password) async
@@ -101,7 +33,7 @@ class AccountApi {
     SharedPreferences prFr =await SharedPreferences.getInstance();///guara waslogin en memeoria
 
     // Uri uri =Uri.http("192.168.43.116:8084/proyectoapiMySql/rest/personapi/log");
-    Uri uri=new Uri(scheme: 'http',host: '192.168.0.3',path: 'proyectoapiMySql/rest/personapi/log',port: 8084);//funciona
+    Uri uri=new Uri(scheme: 'http',host: '192.168.43.116',path: 'proyectoapiMySql/rest/personapi/log',port: 8084);//funciona
     try{
       final http.Response response= await http.post(
           uri,
@@ -118,11 +50,10 @@ class AccountApi {
         //preguntar el codigo de error guardarlo para el mensaje
         //si todo bie  guardar el id del usuario y el nombre tal vez tambien
         //await prFr.setBool("waslogin", true);
-        final  parsed= json.decode(response.body);
-        //_changeValue(parsed);
+        final  parsed= json.decode(response.body); 
         print("login ok ${parsed['idP']}: user: ${parsed['email']}: islog?${parsed['isLog']}");
         if(parsed['isLog']){
-          //_changeValue(parsed);
+          await _changeValue(parsed);
           return true;
         }
         if (parsed['codError']=="0001"){
@@ -142,7 +73,7 @@ class AccountApi {
   }
 
   Future<Map<String, dynamic>> dataUser(String email, String password) async {
-    Uri uri=new Uri(scheme: 'http',host: '192.168.0.3', path: 'proyectoapiMySql/rest/personapi/log', port: 8084);//funciona
+    Uri uri=new Uri(scheme: 'http',host: '192.168.43.116', path: 'proyectoapiMySql/rest/personapi/log', port: 8084);//funciona
     try{
       final http.Response response= await http.post(
           uri,
@@ -174,7 +105,7 @@ class AccountApi {
   }
 
   Future<Map<String, dynamic>> updatePassword(int id , String passwordOld, String passwordNew) async {
-    Uri uri=new Uri(scheme: 'http',host: '192.168.0.3', path: 'proyectoapiMySql/rest/personapi/password', port: 8084);//funciona
+    Uri uri=new Uri(scheme: 'http',host: '192.168.43.116', path: 'proyectoapiMySql/rest/personapi/password', port: 8084);//funciona
     try{
       final http.Response response= await http.post(
           uri,
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,234 +9,7 @@ import 'package:home_resident/widget/my_btn.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'home_page.dart';
 import 'update_password.dart';
-/*
-class LoginPage extends StatefulWidget {
-  static final routeName = 'login';
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
 
-class _LoginPageState extends State<LoginPage> {
-  FocusNode _focusNodePassword = FocusNode();
-  AccountAPI _accountAPI = AccountAPI();
-  GlobalKey<FormState> _formKey = GlobalKey();
-  bool _isFetching = false;
-  String _email = ' ', _password = ' ';
-  int idAlumno = 40;
-  String Curso = "PrimeroTT";
-  _submit() async {
-    final bool isValid = _formKey.currentState.validate();
-    if (isValid) {
-      setState(() {
-        // setState es para volver a renderizar
-        _isFetching = true;
-      });
-      bool isOk = await _accountAPI.login(_email,
-          _password); // como login retorna un bool a futuro usamos await
-      if (isOk) {
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        print("Esto es login page");
-        await prefs.setBool("wasLogin", true);
-        await prefs.setString("Curso", Curso);
-        await prefs.setInt("Alumno", idAlumno);
-        Navigator.pushReplacementNamed(context, HomePage.routeName, arguments: {"alumno": idAlumno,"curso": Curso,});
-      } else {
-        setState(() {
-          _isFetching = false;
-        });
-        await Dialogs.alert(context,
-            title: "Error", body: "El email o contraseña es incorrecto");
-        print("lalala");
-      }
-    }
-  }
-
-  String _validateEmail(String email) {
-    if (email.isNotEmpty && email.contains("@")) {
-      _email = email;
-      print(_email);
-      return null;
-    }
-    return "Invalid Email";
-  }
-
-  String _validatePassword(String password) {
-    if (password.isNotEmpty && password.length > 5) {
-      _password = password;
-      print(_password);
-      return null;
-    }
-    return "Invalid Password";
-  }
-
-
-
-  @override
-  void dispose() {
-    _focusNodePassword.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: SingleChildScrollView(
-                child: Container(
-              height: 900,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  ConstrainedBox(
-                      constraints: BoxConstraints(
-                        maxWidth: 300,
-                        minWidth: 200,
-                      ),
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: <Widget>[
-                            Image.network(
-                                'https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo4.png'),
-                            SizedBox(
-                              height: 40,
-                            ),
-                            // es mas facil describir la logica textFormField que textFied
-                            TextFormField(
-                              decoration: InputDecoration(
-                                hintText: "example@gmail.com",
-                                labelText: "E-mail",
-                                prefixIcon: Container(
-                                  width: 70,
-                                  height: 40,
-                                  padding: EdgeInsets.all(10),
-                                  child: SvgPicture.asset(
-                                      'assets/icons/gmail.svg'),
-                                ),
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                              keyboardAppearance: Brightness.light,
-                              textInputAction: TextInputAction.next,
-                              validator: _validateEmail,
-                              onFieldSubmitted: (String text) {
-                                //Se usa en vez onSubmitted
-                                _focusNodePassword.nextFocus();
-                              },
-                              initialValue: "eve.holt@reqres.in",
-                            ),
-                            SizedBox(height: 10),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                  hintText: "*******",
-                                  labelText: "Password",
-                                  prefixIcon: Icon(Icons.lock),
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(30))),
-                              obscureText: true,
-                              textInputAction: TextInputAction.send,
-                              focusNode: _focusNodePassword,
-                              validator: _validatePassword,
-                              onFieldSubmitted: (String text) {
-                                _submit();
-                              },
-                              initialValue: "cityslicka",
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                CupertinoButton(
-                                  padding: EdgeInsets.zero,
-                                  child: Container(
-                                    padding: EdgeInsets.symmetric(vertical: 5),
-                                    margin: EdgeInsets.symmetric(horizontal: 5),
-                                    child: Text("Recuperar Contraseña"),
-                                    decoration: BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              width: 1, color: Colors.blue)),
-                                    ),
-                                  ),
-                                  onPressed: () {},
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 30,
-                            ),
-                            MyBtn(
-                              fullWidth: true,
-                              label: "Ingresar",
-                              onPressed: _submit,
-                              backgroundColor: Color(0xff6A1B9A),
-                              textColor: Colors.white,
-                            ),
-                            SizedBox(
-                              height: 30,
-                            ),
-                            Text("O inicia con"),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Expanded(
-                                  child: MyBtn(
-                                    fullWidth: true,
-                                    label: "Facebook",
-                                    onPressed: () {},
-                                    backgroundColor: Color(0xff0D47A1),
-                                    textColor: Colors.white,
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: 18, horizontal: 10),
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Expanded(
-                                  child: MyBtn(
-                                    fullWidth: true,
-                                    label: "Google",
-                                    onPressed: () {},
-                                    backgroundColor: Color(0xffF50057),
-                                    textColor: Colors.white,
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: 18, horizontal: 10),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      )),
-                  SizedBox(
-                    height: 30,
-                  )
-                ],
-              ),
-            )),
-          ),
-          if (_isFetching) // cuando es true ejecuta positioned
-            Positioned.fill(
-                child: Container(
-              color: Colors.black26,
-              child: Center(
-                child: CupertinoActivityIndicator(
-                  radius: 15,
-                ), // es el simbolo de cargando
-              ),
-            ))
-        ],
-      )),
-    );
-  }
-}*/
 class LoginPage extends StatefulWidget {
   static final routeName = 'login';
   @override
@@ -266,21 +39,22 @@ class _LoginPageState extends State<LoginPage> {
         _isFetching=true;
       });
       //llama a la api
-      _credencial.loadData(_email, _password);
+      //_credencial.loadData(_email, _password);
       bool isOk=await _accountApi.login(_email,_password);
       if(isOk){
         SharedPreferences prFr =await SharedPreferences.getInstance();///guara waslogin en memeoria
         await prFr.setBool("wasLogin", true);
-        int idAlumno = _credencial.data["alumno"] as int;
-        String curso = _credencial.data["curso"].toString();
-        int rol = _credencial.data["rol"] as int;
-        String name = _credencial.data["name"].toString();
-        String email = _credencial.data["email"].toString();
-        print("El id del alumno es $idAlumno y el curso es $curso");
-        await prFr.setString("Curso", curso);
-        await prFr.setInt("Alumno", idAlumno);
-        await prFr.setString("email", email);
-        await prFr.setString("name", name);
+        int rol =  prFr.getInt("rol") ;
+        int idAlumno =   prFr.getInt("Alumno");
+        String curso =  prFr.getString("Curso");
+        String name =  prFr.getString("name");
+        String email = prFr.getString("email").toString();
+        print("trae datos?");
+        print(rol);
+        print(idAlumno);
+        print(curso);
+        print(name);
+        print(email);
         if(rol == 4) {
           Navigator.pushReplacementNamed(context, UpdatePassword.routeName, arguments: {"id":idAlumno, "curso":curso,"rol":rol},);
         }else{
